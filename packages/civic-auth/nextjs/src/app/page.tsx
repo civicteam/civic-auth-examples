9637f795674f76274e887729cf5288ff31e8a0ac
dankelleher <dankelleher@yahoo.com>
2025-02-27 08:28:32 +0100

    TECH-1535 Remove use client and simplify example

--- packages/civic-auth/nextjs/src/app/page.tsx
+++ packages/civic-auth/nextjs/src/app/page.tsx
@@ -1,17 +1,14 @@
 import { getUser } from '@civic/auth/nextjs';
-import { Login } from "./login";
+import { UserButton } from "@civic/auth/react";
 
 export default async function Home() {
   const user = await getUser();
   return (
-    <div>
       <main>
           {user && (
             <div>Hello {user.email}</div>
           ) }
-          <Login />
+          <UserButton />
       </main>
-     
-    </div>
   );
 }
