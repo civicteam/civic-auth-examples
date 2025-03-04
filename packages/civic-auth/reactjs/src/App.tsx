7d662202ecd778762cab3f2605309ad9f6980dfb
Kevin Colgan <kevin@civic.com>
2025-03-04 15:56:04 +0100

    kc_test: Added fix: use sdk beta

--- packages/civic-auth/reactjs/src/App.tsx
+++ packages/civic-auth/reactjs/src/App.tsx
@@ -1,10 +1,12 @@
 import { CivicAuthProvider, UserButton } from "@civic/auth/react";
 
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const AUTH_SERVER = import.meta.env.VITE_AUTH_SERVER;
 const App = () => {
   return (
       <CivicAuthProvider
         clientId={CLIENT_ID}
+        config={{ oauthServer: AUTH_SERVER }}
       >
         <UserButton/>
       </CivicAuthProvider>
