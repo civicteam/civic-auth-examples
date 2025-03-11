9ca93af179ad1a6c30323d5f09ac26bbf98a751c
“mitchcivic” <mitch@civic.com>
2025-03-11 11:40:36 -0700

    hardcode string

--- packages/civic-auth/nextjs/next.config.ts
+++ packages/civic-auth/nextjs/next.config.ts
@@ -1,11 +1,12 @@
-import type { NextConfig } from "next";
 import { createCivicAuthPlugin } from "@civic/auth/nextjs"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
 const withCivicAuth = createCivicAuthPlugin({
-  clientId: 'prod-demo-client-1'
+  clientId: "prod-demo-client-1"
 });
 
-export default withCivicAuth(nextConfig);
+export default withCivicAuth(nextConfig)
