76a0c3ecdf7d13982273bdb15c160e36493a4bc8
“mitchcivic” <mitch@civic.com>
2025-03-11 12:57:54 -0700

    do not start nextjs app

--- packages/civic-auth/nextjs/src/app/layout.tsx
+++ packages/civic-auth/nextjs/src/app/layout.tsx
@@ -1,18 +1,10 @@
 import { CivicAuthProvider } from "@civic/auth/nextjs";
-import type { ReactNode } from 'react';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: ReactNode;
-}>) {
+function Layout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <CivicAuthProvider>
-          {children}
-        </CivicAuthProvider>
-      </body>
-    </html>
-  );
+    <CivicAuthProvider>
+      {children}
+    </CivicAuthProvider>
+    ...
+  )
 }
--- packages/civic-auth/nextjs/src/middleware.ts
+++ packages/civic-auth/nextjs/src/middleware.ts
@@ -1,5 +1,7 @@
 import { authMiddleware } from '@civic/auth/nextjs/middleware'
+
 export default authMiddleware();
+
 export const config = {
   // include the paths you wish to secure here
   matcher: [
