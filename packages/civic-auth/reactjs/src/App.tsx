5555c7f9d25e41879f687c2f5143680776336644
“mitchcivic” <mitch@civic.com>
2025-03-23 18:12:42 -0700

    update tests

--- packages/civic-auth/reactjs/src/App.tsx
+++ packages/civic-auth/reactjs/src/App.tsx
@@ -1,15 +1,13 @@
 import { CivicAuthProvider, UserButton } from "@civic/auth/react";
 
 const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
-const AUTH_SERVER = import.meta.env.AUTH_SERVER;
-// const PORT = import.meta.env.PORT ? parseInt(import.meta.env.PORT) : 3000;
+const AUTH_SERVER = import.meta.env.VITE_AUTH_SERVER;
 
 function App() {
   return (
     <CivicAuthProvider 
       clientId={CLIENT_ID}
       config={{ oauthServer: AUTH_SERVER }}
-      redirectUrl='http://localhost:3000/api/auth/callback'
     >
       <UserButton />
     </CivicAuthProvider>
