be23ecf8a7694069d6d5d6d1b58b2c857425cc0d
Flippie Scholtz <flippie@civic.com>
2025-04-28 09:50:19 +0200

    Attempt a custom 'callbackUrl' in the NextHS example

--- packages/civic-auth/nextjs/next.config.ts
+++ packages/civic-auth/nextjs/next.config.ts
@@ -6,7 +6,8 @@ const nextConfig: NextConfig = {
 };
 
 const withCivicAuth = createCivicAuthPlugin({
-  clientId: `${process.env.CLIENT_ID}`
+  clientId: `${process.env.CLIENT_ID || "78f0bf99-cc9a-45b3-b0e9-a9498fb386c7"}`,
+  callbackUrl: '/api/myCustomCivicRoute'
 });
 
 export default withCivicAuth(nextConfig)
