d21039a1778173cbe7af5134837c8093386ba148
“mitchcivic” <mitch@civic.com>
2025-04-30 12:02:56 -0700

    fix test

--- packages/civic-auth/nextjs/next.config.ts
+++ packages/civic-auth/nextjs/next.config.ts
@@ -2,12 +2,10 @@ import { createCivicAuthPlugin } from "@civic/auth/nextjs"
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.BASE_PATH || '',
 };
 
 const withCivicAuth = createCivicAuthPlugin({
   clientId: `${process.env.CLIENT_ID}`,
-  basePath: process.env.BASE_PATH || '',
 });
 
 export default withCivicAuth(nextConfig)
