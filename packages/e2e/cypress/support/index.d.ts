42a7370345a949484a4b19bd35ff72bcb0c685b7
“mitchcivic” <mitch@civic.com>
2025-07-09 07:45:56 -0700

    undo adding file

--- packages/e2e/cypress/support/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-  interface Chainable {
-    iframe(selector: string): Chainable<JQuery<HTMLElement>>;
-    frameLoaded(selector: string): Chainable<void>;
-    enter(selector: string): Chainable<JQuery<HTMLElement>>;
-  }
-} 
