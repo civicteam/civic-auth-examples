c0dd00f868fe5ce3f2e3b10f1afd12f7618000ba
Christiano Teixeira <christiano@cteixeira.com.br>
2025-09-04 08:59:11 -0300

    chore: cleanup

--- packages/mobile/react-native-expo/components/Providers.tsx
+++ packages/mobile/react-native-expo/components/Providers.tsx
@@ -1,14 +1,5 @@
 import { AuthProvider } from "@/contexts/AuthContext";
-import { ErrorBoundary } from "./ErrorBoundary";
-// import { WagmiProvider } from "wagmi";
-// import { wagmiConfig } from "@/config/wagmi";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <ErrorBoundary>
-      {/*<WagmiProvider config={wagmiConfig}>*/}
-      <AuthProvider>{children}</AuthProvider>
-      {/*</WagmiProvider>*/}
-    </ErrorBoundary>
-  );
+  return <AuthProvider>{children}</AuthProvider>;
 };
