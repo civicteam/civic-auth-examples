0304b8ced8cc5561b7fc14ad0fcf3b6f55c7703c
“mitchcivic” <mitch@civic.com>
2025-11-17 18:19:37 -0800

    one more time

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -79,7 +79,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
     
     /* Set default timeout for all actions to 30 seconds */
     actionTimeout: 30000,
